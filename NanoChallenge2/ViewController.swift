55ca5d97cef3f5c9d580a0fd2784e49552d3c61e
arnoldtjiawi <arnoldtjiawi@gmail.com>
2019-09-19 10:26:32 +0700

    Initial Commit

---
+++ NanoChallenge2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoChallenge2
+//
+//  Created by Arnold Tjiawi on 19/09/19.
+//  Copyright © 2019 ArnoldTjiawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
